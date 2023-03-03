0c9cc876c5382faf838b4648478862dd0aca4201
michele <michele.giovagnoli1@gmail.com>
2023-03-03 18:41:14 +0100

    BONUS prova

--- js/main.js
+++ js/main.js
@@ -7,29 +7,67 @@
 const domBtn = document.getElementById('btn');
 domBtn.addEventListener('click',
     function(){
+        
         const domGriglia = document.getElementById('griglia');
-
-        for( let i = 0; i < 100; i++) {
-
-            const box = newBox(i+1);
-            domGriglia.append(box);
-
-            box.addEventListener('click',
-               function(){
-                this.classList.toggle('click');
-                console.log(box.innerHTML);
-               }
-            
-            );
+        let domDifficolta = document.getElementById('difficolta');
+        console.log(domDifficolta.value);
+         
+        if(domDifficolta.value == 0) {
+            for( let i = 0; i < 100; i++) {
+                const box = newBox_0(i+1);
+                domGriglia.append(box);
+                box.addEventListener('click',
+                    function(){
+                        this.classList.toggle('click');
+                        console.log(box.innerHTML);
+                    }
+                );
+            }
         }
+        else if (domDifficolta.value == 1) {
+            for( let i = 0; i < 81; i++) {
+                const box = newBox_1(i+1);
+                domGriglia.append(box);
+                box.addEventListener('click',
+                    function(){
+                        this.classList.toggle('click');
+                        console.log(box.innerHTML);
+                    }
+                );
+            }
+        }
+        else if (domDifficolta.value == 2) {
+            for( let i = 0; i < 49; i++) {
+                const box = newBox_2(i+1);
+                domGriglia.append(box);
 
+                box.addEventListener('click',
+                    function(){
+                        this.classList.toggle('click');
+                        console.log(box.innerHTML);
+                    }
+                );
+            }
+        }
     }
 );
 
 
-function newBox(numero){
+function newBox_0(numero){
     const box = document.createElement('div');
     box.classList.add('box');
+    box.innerHTML = numero;
+    return box;
+}
+function newBox_1(numero){
+    const box = document.createElement('div');
+    box.classList.add('box-1');
+    box.innerHTML = numero;
+    return box;
+}
+function newBox_2(numero){
+    const box = document.createElement('div');
+    box.classList.add('box-2');
     box.innerHTML = numero;
     return box;
 }
