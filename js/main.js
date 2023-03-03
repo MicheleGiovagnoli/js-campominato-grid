a0da876c36308f7f56ea43449248c1d0e178b602
michele <michele.giovagnoli1@gmail.com>
2023-03-03 17:39:39 +0100

    Le caselle se cliccate si colorano di azzurro e stampano il numero nella console

--- js/main.js
+++ js/main.js
@@ -9,21 +9,28 @@ domBtn.addEventListener('click',
     function(){
         const domGriglia = document.getElementById('griglia');
 
-
         for( let i = 0; i < 100; i++) {
 
-            const box = newBox();
+            const box = newBox(i+1);
             domGriglia.append(box);
 
+            box.addEventListener('click',
+               function(){
+                this.classList.toggle('click');
+                console.log(box.innerHTML);
+               }
+            
+            );
         }
 
     }
 );
 
 
-function newBox(){
+function newBox(numero){
     const box = document.createElement('div');
     box.classList.add('box');
+    box.innerHTML = numero;
     return box;
 }
 
